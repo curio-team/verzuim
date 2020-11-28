f49d93cd35827de788f811638b36d3aa517140da
Bart Roos <bart@studiokaa.co>
2020-11-28 18:31:12 +0100

    Update cog icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                         </ul>
                         <div class="btn-group d-none d-md-flex">
                             @yield('buttons')
-                            <a class="btn btn-outline-dark" href="{{ route('settings.show') }}"><i class="fas fa-user fa-fw" aria-hidden="true"></i>&nbsp;<span>{{ Auth::user()->id }}</span></a>
+                            <a class="btn btn-outline-dark" href="{{ route('settings.show') }}"><i class="fas fa-cog fa-fw" aria-hidden="true"></i></a>
                         </div>
                     </div>
                 @endif
